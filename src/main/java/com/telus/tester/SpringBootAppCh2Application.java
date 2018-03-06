6402ff50314b5e741b9c4d4708bbfd54feb84359
Nelson Lau <nelson.lau@telus.com>
2018-03-05 22:40:31 -0500

    init checkin

---
+++ src/main/java/com/telus/tester/SpringBootAppCh2Application.java
@@ -0,0 +1,12 @@
+package com.telus.tester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppCh2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppCh2Application.class, args);
+	}
+}
